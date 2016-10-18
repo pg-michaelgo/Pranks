e3426c12c0cd453a37f6ca0093de92ef0d4076d3
ggmichaelgo <ggmichaelgo@gmail.com>
2016-10-18 15:45:47 -0300

    more executable script for faster pranking action

---
+++ random_website.sh
@@ -0,0 +1,13 @@
+osascript -e '
+repeat
+  set delay_min to (random number from 1 to 6)
+  set delay_min to delay_min * 5 * 60
+
+  do shell script "sleep " & delay_min
+
+  set url_list to {"http://www.facebook.com", "http://www.google.ca"}
+  set r_url to some item of url_list
+
+  open location r_url
+end repeat
+' &
