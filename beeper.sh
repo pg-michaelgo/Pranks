b0d2f08fbdbbff39bbfcec9b3c4e79764d6e282e
pg-michaelgo <mgo@propertyguys.com>
2016-11-10 16:00:50 -0400

    Update beeper.sh

--- beeper.sh
+++ beeper.sh
@@ -1,17 +1,15 @@
 osascript -e '
 repeat
-  set delay_min to (random number from 1 to 6)
-  set delay_min to delay_min * 5 * 60
-
-  set startTime to current date
-  do shell script "sleep " & delay_min
-
-  set sound_list to {"afplay ~/../../../System/Library/Sounds/Funk.aiff", "afplay ~/../../../System/Library/Sounds/Basso.aiff", "afplay ~/../../../System/Library/Sounds/Funk.aiff"}
-  set sound_file to some item of sound_list
-
-  set r to random number from 1 to 2
-  repeat r times
-    do shell script sound_file
-  end repeat
+	set delay_seconds to (random number from 30 to 60)
+	set startTime to current date
+	do shell script "sleep " & delay_seconds
+	set sound_list to {"afplay ~/../../../System/Library/Sounds/Funk.aiff", "afplay ~/../../../System/Library/Sounds/Basso.aiff", "afplay ~/../../../System/Library/Sounds/Glass.aiff", "afplay ~/../../../System/Library/Sounds/Morse.aiff"}
+	set sound_file to some item of sound_list
+	set r to random number from 1 to 2
+	repeat r times
+		set volume_value to (random number from 70 to 100)
+		set volume output volume volume_value --100%
+		do shell script sound_file
+	end repeat
 end repeat
 ' &
