5449cf43711b0c2838fa49ca164b689331078f2e
pg-michaelgo <mgo@propertyguys.com>
2016-12-20 15:45:07 -0400

    Update random_website.sh

--- random_website.sh
+++ random_website.sh
@@ -1,11 +1,11 @@
-osascript -e '
+nohup osascript -e '
 repeat
   set delay_min to (random number from 1 to 6)
   set delay_min to delay_min * 5 * 60
 
   do shell script "sleep " & delay_min
 
-  set url_list to {"http://www.facebook.com", "http://www.google.ca"}
+  set url_list to {"https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
   set r_url to some item of url_list
 
   open location r_url
